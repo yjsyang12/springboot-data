5c40a0cb38ba5cdda9fda6b15c14450dd9ed91f5
yanzhongjian <2f277920>
2021-06-09 18:15:53 +0800

    搭建环境

---
+++ src/test/java/com/young/springbootdata/SpringbootDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.young.springbootdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
